3438f29c4ea12baff547206b997be8f54f24ebae
jonathanmurmu <jonathanmurmu@gmail.com>
2016-03-02 16:21:15 +0530

    added loading image on scrap and dashboard page

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,23 @@ $(document).ready(function() {
             data: $(this).serialize(), // get the form data
             type: $(this).attr('method'), // GET or POST
             url: $(this).attr('action'), // the file to call
+            beforeSend: function(){
+                // clearing the product result area.
+                $('result_area').html('')           
 
+                // displaying the loading image
+                $("#load_div").show();
+
+                // disabling the submit button
+                $('#dashboard_search_submit').addClass("disabled")
+            },
+            complete: function(){
+                // when the products are listed remove the loading image
+                $("#load_div").hide();
+
+                // enable the submit button again
+                $('#dashboard_search_submit').removeClass("disabled")
+            },
             success: function(json_data) { // on success..
             	$('#result_area').html(json_data.result)
             },
@@ -19,7 +35,23 @@ $(document).ready(function() {
             data: $(this).serialize(), // get the form data
             type: $(this).attr('method'), // GET or POST
             url: $(this).attr('action'), // the file to call
+            beforeSend: function(){
+                // clearing the product result area.
+                $('#scrap_result_area').html('')                
 
+                // displaying the loading image
+                $("#load_div").show();
+
+                // disabling the submit button
+                $('#scrap_submit').addClass("disabled")
+            },
+            complete: function(){
+                // when the products are listed remove the loading image
+                $("#load_div").hide();
+
+                // enable the submit button again
+                $('#scrap_submit').removeClass("disabled")
+            },
             success: function(json_data) { // on success..
                 $('#scrap_result_area').html(json_data.result)
             },
